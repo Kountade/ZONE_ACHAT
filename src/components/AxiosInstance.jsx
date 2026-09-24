28ac239858d9890808775aa3819dee7c4935feaa
Codons-ensemble <bounamakountagoudiaby@gmail.com>
2026-09-24 15:39:09 +0100

    first commit host

--- src/components/AxiosInstance.jsx
+++ src/components/AxiosInstance.jsx
@@ -11,7 +11,7 @@ const getBaseUrl = () => {
   
   // Priorité 2 : Détection selon le mode
   if (import.meta.env.PROD) {
-    return 'https://sodepci-backend.onrender.com'
+    return 'https://zone-achat-backend.onrender.com'
   }
   
   // Développement local
